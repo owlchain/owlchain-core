cb3f31eac915864b9969ef0486817bf6bb2ce2ee
Knolza <knolza2016@gmail.com>
2016-12-28 01:57:11 +0900

    Adding web ui component css, fonts and javascripts

--- source/owlchain/ui/app.d
+++ source/owlchain/ui/app.d
@@ -1,24 +1,77 @@
 module owlchain.ui.app;
 
 import vibe.d;
+import vibe.utils.array;
+
 
 shared static this()
 {
-	import std.stdio:writeln;
-	writeln("hello owlchain");
-}
+	auto router = new URLRouter;
+	router.get("/", staticRedirect("index.html"));
+	router.get("/ws", handleWebSockets(&handleWebSocketConnection));
+	router.get("*", serveStaticFiles("public/"));
 
-shared static this()
-{
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
-	listenHTTP(settings, &hello);
+	listenHTTP(settings, router);
 
 	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
 }
 
-void hello(HTTPServerRequest req, HTTPServerResponse res)
+private WebSocket[] sockets;
+
+void handleWebSocketConnection(scope WebSocket socket)
 {
-	res.writeBody("Hello, World!");
+	sockets ~= socket;
+	socket.waitForData(1.seconds);
+	string name = socket.receiveText;
+
+	if(name !is null) 
+	{
+		logInfo("%s connected @ %s.", name, socket.request.peer);
+		sendTextToOtherClients(null, "System", name ~ " connected to the chat.");
+	}
+	else
+	{
+		socket.send("{\"name\":\"System\", \"text\":\"Invalid name.\"}");
+		socket.close;
+		sockets.removeFromArray!WebSocket(socket);
+		logInfo("%s disconnected.", name);
+		return;
+	}
+
+	while (socket.waitForData)
+	{   
+		if (!socket.connected) break;
+        // we got something
+        auto text = socket.receiveText;
+ 
+        // Close if recieve "/close"
+        if (text == "/close") break;
+ 
+        logInfo("Received: \"%s\" from %s.", text, name);
+        // Relay text to everyone else
+        sendTextToOtherClients(socket, name, text);
+    }
+ 
+    // Remove socket from sockets list and close socket
+    socket.close;
+    sockets.removeFromArray!WebSocket(socket);
+    logInfo("%s disconnected.", name);
+ 
+    sendTextToOtherClients(null, "System", name ~ " disconnected to the chat.");
+}
+
+void sendTextToOtherClients(scope WebSocket src_socket, string name, string text)
+{
+    foreach (socket; sockets)
+    {
+        // Don't send it to people who won't get it.
+        if (!socket.connected) continue;
+ 
+        logInfo("Sending: \"%s\" to %s.", text, socket.request.peer);
+        // JSON encoding for simplicity
+        socket.send("{\"name\":\"" ~ name ~ "\", \"text\":\"" ~ text ~ "\"}");
+    }
 }
