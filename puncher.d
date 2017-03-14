b50fdfb5e3984cc03456f9c9a9afc7dc965315bf
astonshnam <drumhyun@naver.com>
2017-03-14 15:36:59 +0900

    Create puncher.d
    Create puncher.d

--- puncher.d
+++ puncher.d
@@ -2,11 +2,14 @@
 
 import std.stdio;
 import std.getopt;
+import std.string;
+import std.file;
+import std.array;
 
 immutable enum VER = 0.1;
 
 immutable string SYNOPSIS = `
-License punching utilility for program source file.
+License punching utility for program source file.
 
 [synopsis]
 
@@ -25,18 +28,121 @@ at the root of this distribution or at https://www.gnu.org/licenses/gpl-3.0.html
 
 
 int main(string[] args){
-	string license ;
-	bool recursive=false;
-
-	auto helpInformation = getopt(
-    args,
-    "r|recursive", &recursive);
-
-	if (helpInformation.helpWanted)
+	version(Windows)
 	{
-		defaultGetoptPrinter("License punching utilility for program source file.",
-	  		helpInformation.options);
+		writeln("This program does not work on Windows.");
+	}
+	else
+	{
+		string license ;
+		bool recursive = false;
+
+		auto helpInformation = getopt(
+	    args,
+	    "r|recursive", &recursive);
+
+		if (helpInformation.helpWanted)
+		{
+			defaultGetoptPrinter("License punching utilility for program source file.",
+		  		helpInformation.options);
+		}
+
+		if(!recursive)
+		{
+			foreach(i, arg; args[1..$])
+			{
+				if(arg[$-2..$] != ".d")
+				{
+					writeln("The type of ", arg, " is not '*.d'.");
+				}
+				else
+				{
+					punchFile(arg);
+				}
+			}
+		}
+		else
+		{
+			auto dFiles = dirEntries("", "*.{d,di}", SpanMode.depth);
+			foreach (d; dFiles)
+			{
+	    		punchFile(d.name);
+	    	}
+		}
+	}
+	return 0;
+}
+
+void punchFile(string fileName)
+{
+	if (fileName[0..7] == "backup/")
+	{
+		return;
+	}
+	string buffer = readFile(fileName);
+	if(checkBuffer(buffer, fileName))
+	{
+		copyFile(fileName);
+		writeFile(buffer, fileName);
+	}				
+}	
+
+string readFile(string fileName)
+{
+	string buffer;
+	File file = File(fileName, "r");
+				
+	foreach(line; file.byLine)
+	{
+		buffer ~= line;
+		buffer ~= "\n";
 	}
 
-	return 0;
+	file.close();
+
+	return buffer;
+}
+
+void writeFile(string buffer, string fileName)
+{
+	File file = File(fileName, "w");
+	file.writeln(SOURCE_HEADER, "\n");
+	file.writeln(buffer);
+	file.close();
+
+	writeln(fileName, " is complety punched.");
+}
+
+bool checkBuffer(string buffer, string fileName)
+{
+	if(buffer.length > 19 && buffer[0..19] == "#!/usr/bin/env rdmd")
+	{
+		writeln("You can not punch a shell script file(", fileName, ").");
+		return false;
+	}
+
+	if(buffer.length > 56 && buffer[0..56] == SOURCE_HEADER[0..56]) // SOURCE_HEADER[0..56] == /*\nCopyright 2016 Owlchain communities and contributors.
+	{
+		writeln(fileName, " is already punched.");
+		return false;
+	}
+
+	return true;
+}
+
+void copyFile(string fileName)
+{
+	string path = "backup/";
+
+	foreach(s; split(fileName, "/")[0..$-1])
+	{
+		path = path ~ s ~ "/";
+	}
+
+	if (!exists(path))
+	{
+		mkdirRecurse(path.dup);
+	}
+	
+	copy(fileName, "backup/" ~ fileName);
 }
