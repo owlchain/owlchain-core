607c0668d40feea54faa66f6673649a258ef1391
yeshtml5 <yeshtml5@gmail.com>
2017-03-21 16:07:41 +0900

    add

---
+++ source/owlchain/ui/public/js/function.js
@@ -0,0 +1,19 @@
+/*
+ 
+ */
+(function ($) {
+    "use strict";
+    /*CONST*/
+    
+
+    $.FUNC = {};
+    $.FUNC.init = function () {
+
+        $.FUNC.setup();
+    };
+
+    // 나의메뉴 스크립트(개발 ajax 호출 콜백에서 실행)
+    $.FUNC.setup = function () {
+
+    };
+}($));
