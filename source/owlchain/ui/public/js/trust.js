bbc275469ce14b385aca64a6626676d065ba9aa2
yeshtml5 <yeshtml5@gmail.com>
2017-04-02 13:18:28 +0900

    trust-contract addon

--- source/owlchain/ui/public/js/trust.js
+++ source/owlchain/ui/public/js/trust.js
@@ -8,9 +8,19 @@ trust.js
     };
     /*trust-contract*/
     var _bind = function () {
-        function setDisplay(cls) {
+        function setDisplay(cls) { 
+            //  console.log('cls: '+cls);
             _section.hide();
-            _section.siblings('.' + cls).show()
+            _section.siblings('.' + cls).show();
+            _menu.val(cls);
+            var _t = _section.siblings('.' + cls);
+            if (cls == "s2") { 
+                setTimeout(function () {
+                    _t.find('.list').addClass('on');
+                }, 2000);
+            } else {
+            }
+            //$("#selectBox option:eq(2)").attr("selected", "selected");
         }
         var _menu = $('header select');
         var _artivle
@@ -18,17 +28,22 @@ trust.js
         var _link = $('a.link');
 
         _menu.bind('change', function (event) {
-            console.log($(this).val());
             var _val = $(this).val();
             setDisplay(_val);
         });
-
-
+        /*공통UI*/
         _link.bind('click', function (event) {
             var _val = $(this).attr('data-val');
-             setDisplay(_val);
-
+            setDisplay(_val);
         });
+        /**/
+        $('.popup-link').bind('click', function (event) {
+            var _popup = $('.s1 .popup');
+            _popup.addClass('on');
+        });
+        /*init*/
+        var _cls = $('section.on').attr('class').split('on')[0].replace(/\s+/g, '');
+        setDisplay(_cls);
     }
     /*ADD_ACCOUNT
      */
