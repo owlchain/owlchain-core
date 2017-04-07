19fce2f122842b6b21703401fc8d601a4ef350a0
astonshnam <drumhyun@naver.com>
2017-04-07 17:22:46 +0900

    Modify wallet.d
    Add class Account

--- source/owlchain/wallet/wallet.d
+++ source/owlchain/wallet/wallet.d
@@ -2,17 +2,62 @@ module owlchain.wallet.wallet;
 
 import owlchain.api.api;
 
-class Wallet : IWallet {
-    IAccount getAccount() {
+class Wallet : IWallet
+{
+    IAccount getAccount()
+    {
         return null;
     }
-    string createSeed() {
+    
+    string createSeed()
+    {
         return null;
     }
-    bool removeAccount(IAddress address) {
+    
+    bool removeAccount(IAddress address)
+    {
         return true;
     }
-    bool removeAccount(IAccount account) {
+    
+    bool removeAccount(IAccount account)
+    {
+        return true;
+    }
+}
+
+class Account {
+    IAddress getAddress()
+    {
+        return null;
+    }
+    
+    string getAlias()
+    {
+        return null;
+    }
+    
+    void setAlias(AccountAlias aliasName)
+    {
+        
+    }
+    
+    ulong getBalance()
+    {
+        return 0;
+    }
+    
+    bool send(ulong amount, IAccount receiver)
+    {
+        return true;
+    }
+    
+    bool send(ulong amount, IAddress address)
+    {
+        return true;
+    }
+    
+    bool setFreeze(ulong amount)
+    {
         return true;
     }
 }
