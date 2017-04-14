e759c52197fa510f71d2841d71a5ec9daa6b9b94
yeshtml5 <yeshtml5@gmail.com>
2017-04-15 00:06:15 +0900

    Vocabulary

--- source/owlchain/ui/public/pages/js/common.js
+++ source/owlchain/ui/public/pages/js/common.js
@@ -2,6 +2,7 @@
 trust.js
 *************************/
 (function($) {
+    "use strict";
     /*setup*/
     var _setup = function() {
 
@@ -9,33 +10,27 @@ trust.js
     /*trust-contract*/
     var _bind = function() {
         var _isMotion = false;
+
         function setDisplay(cls) {
             var _old = _section.siblings('.on');
             var _sec = _section.siblings('.' + cls);
-            //  _section.removeClass('on');  bounceInRight  bounceOutLeft
-            _isMotion=true;
-            _sec.addClass('on bounceInRight animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            _isMotion = true;
+            _sec.show().addClass('on bounceInRight animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
                 $(this).removeClass('bounceInRight animated');
-                _isMotion=false;
+                _isMotion = false;
             });
             _old.addClass('bounceOutLeft animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-                $(this).removeClass('on bounceOutLeft animated');
+                $(this).removeClass('on bounceOutLeft animated').hide();
             });
             _menu.val(cls);
-            var _t = _section.siblings('.' + cls);
-            if (cls == "s1") {
-                $('.CodeMirror').focus();
-            } else if (cls == "s2") {
-                setTimeout(function() {
-                    _t.find('.list').addClass('on');
-                }, 2000);
-            } else {}
-            //$(window).trigger('resize');
-            //$("#selectBox option:eq(2)").attr("selected", "selected");
-        }
+
+            //  var _height = _sec.height();
+        };
         var _menu = $('header select');
         var _section = $('article.wrap > section');
         var _link = $('a.link');
+        var _winPop = $('a.winPop');
+
 
         _menu.bind('change', function(event) {
             var _val = $(this).val();
@@ -43,7 +38,7 @@ trust.js
         });
         /*공통UI*/
         _link.bind('click', function(event) {
-            if(_isMotion){
+            if (_isMotion) {
                 return false;
             }
             var _val = $(this).attr('data-val');
@@ -52,6 +47,9 @@ trust.js
                 setDisplay(_val);
                 $(this).addClass('on').siblings('a').removeClass('on');
             }
+        });
+        _winPop.bind('click',function(event){
+            window.open("vocabulary.html", "windowNewPop", "toolbar=yes,scrollbars=yes,resizable=yes,top=0,left=0,width=1000,height=800");
         });
         /**/
         $('.popup-link').bind('click', function(event) {
