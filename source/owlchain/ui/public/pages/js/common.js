733695d6178fb508cb7c89f8d49aa53ae02ffd39
yeshtml5 <yeshtml5@gmail.com>
2017-04-16 01:19:05 +0900

    add_publishing

--- source/owlchain/ui/public/pages/js/common.js
+++ source/owlchain/ui/public/pages/js/common.js
@@ -10,7 +10,9 @@ trust.js
     /*trust-contract*/
     var _bind = function() {
         var _isMotion = false;
-
+        /*************************
+        화면전환될때 슬라이드
+        *************************/
         function setDisplay(cls) {
             var _old = _section.siblings('.on');
             var _sec = _section.siblings('.' + cls);
@@ -23,14 +25,32 @@ trust.js
                 $(this).removeClass('on bounceOutLeft animated').hide();
             });
             _menu.val(cls);
-
-            //  var _height = _sec.height();
+            var _height = _sec.height();
+            console.log(_height);
         };
+        /*************************
+        팝업 슬라이드
+        *************************/
+        function showPopup(cls){
+            _popup.addClass('on zoomInRight animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+                $(this).removeClass('zoomInRight animated');
+
+            });
+
+        }
+
+        function hidePopup(cls){
+            _popup.addClass(' zoomOutLeft animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+                $(this).removeClass('zoomOutLeft animated on');
+
+            });
+        }
+
         var _menu = $('header select');
         var _section = $('article.wrap > section');
         var _link = $('a.link');
-        var _winPop = $('a.winPop');
-
+        var _winPop = $('a.winPop');    /* windows popup */
+        var _popup = $('article.popup');
 
         _menu.bind('change', function(event) {
             var _val = $(this).val();
@@ -53,18 +73,22 @@ trust.js
         });
         /**/
         $('.popup-link').bind('click', function(event) {
-            var _popup = $('.s1 .popup');
-            _popup.addClass('on');
+            showPopup();
+        });
+        _popup.bind('click',function(event){
+            hidePopup();
         });
         $('.toggle dl dt').bind('click', function(event) {
             $(this).parents('dl').toggleClass('on');
         });
 
         /*init*/
-        var _cls = $('section.on').attr('class').split('on')[0].replace(/\s+/g, '');
-        $('section.on').addClass('on bounceInRight animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-            $(this).removeClass('bounceInRight animated');
-        });
+        if( $('section.on').length>0){
+            var _cls = $('section.on').attr('class').split('on')[0].replace(/\s+/g, '');
+            $('section.on').addClass('on bounceInRight animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+                $(this).removeClass('bounceInRight animated');
+            });
+        }
         //setDisplay(_cls);
     }
     /*ADD_ACCOUNT
