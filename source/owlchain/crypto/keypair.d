cf2561aff5a8a514f6ddffcfc65288a81e89f9db
Knolza <knolza2016@gmail.com>
2017-04-30 00:18:55 +0900

    Add keypair.d

---
+++ source/owlchain/crypto/keypair.d
@@ -0,0 +1,48 @@
+module owlchain.crypto.keypair;
+
+import wrapper.sodium;
+import std.exception : enforce;
+import std.typecons : tuple, Tuple;
+
+alias KeyPair = Tuple!(ubyte[crypto_box_PUBLICKEYBYTES], "publicKey",
+        ubyte[crypto_box_SECRETKEYBYTES], "secretKey");
+
+bool keypair(out KeyPair kp)
+{
+    return crypto_box_keypair(kp.publicKey, kp.secretKey);
+}
+
+alias SeedBytes = ubyte[crypto_box_SEEDBYTES];
+bool keypairWithSeed(out KeyPair kp, scope SeedBytes seed)
+{
+    return crypto_box_seed_keypair(kp.publicKey, kp.secretKey, seed);
+}
+
+@system 
+unittest
+{
+    import std.stdio:writefln;
+    import std.conv:to;
+ 
+    KeyPair kp;
+    assert ( keypair(kp) == true );
+    writefln("keypair(pk=%s, sk=%s", to!string(kp.publicKey), to!string(kp.secretKey));
+
+    KeyPair kp2;
+    SeedBytes seed = ubyte.init;
+    assert ( keypairWithSeed(kp2, seed) == true);
+    writefln("keypairWithSeed(pk=%s, sk=%s, seed=%s", to!string(kp.publicKey), to!string(kp.secretKey), to!string(seed));
+
+    writefln("passs KeyPair Test");
+}
+
+/++
+usage
+import owlchain.crypto.keypair;
+
+  auto kp = keypair();
+  auto private = kp.privateKey();
+  auto public = kp.publicKye();
+
+  ...
++/
