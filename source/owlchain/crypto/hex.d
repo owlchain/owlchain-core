580a7b5124d588191768f194db41e7bd90eaca4e
Knolza <knolza2016@gmail.com>
2017-05-01 15:45:52 +0900

    add hex module

--- source/owlchain/crypto/hex.d
+++ source/owlchain/crypto/hex.d
@@ -1,15 +1,17 @@
 module owlchain.cryto.hex;
 
 import std.exception:enforce;
-import owlchain.utils.types:uint256,UINT256_BYTE_LENGTH;
+import owlchain.base.types:uint256;
 import wrapper.sodium.utils:sodium_hex2bin,sodium_bin2hex;
+import std.stdio:writefln;
 
-string binToHex(scope ubyte[] bin)
+string binToHex(in ubyte[] bin)
 {
     return sodium_bin2hex(bin);
 }
+alias toHex = binToHex;
 
-string hexAbbrev(scope ubyte[] bin)
+string hexAbbrev(in ubyte[] bin)
 {
     size_t sz = bin.length;
     if (sz > 3)
@@ -19,42 +21,39 @@ string hexAbbrev(scope ubyte[] bin)
     return binToHex(bin[0 .. sz]);
 }
 
-ubyte[] hexToBin(scope string hex,string ignore=null)
+ubyte[] hexToBin(in string hex,string ignore=null)
 {
-    ubyte[] bin = new ubyte[]((hex.length/2) + 1);
+    ubyte[] bin = new ubyte[hex.length];
     size_t  bin_len;
     string  hex_end;
-
     enforce(sodium_hex2bin(bin, hex, ignore, bin_len, hex_end) == 0);
-
+    bin = bin[0 .. bin_len];
     return bin;
 }
+alias toBin = hexToBin;
 
-uint256 hexToBin256(scope string hex) 
-in 
-{
-    enforce(hex.length/2 == UINT256_BYTE_LENGTH);
-}
-body
-{
-    uint256 result;
-    result = hexToBin(hex);
-    return result;
-}
 
 @system
 unittest{
-    import std.stdio:writefln;
-    string[] hexData = [
-        "1234567890",
-        "1234567890abcdf",
-        "1234567890ABCDF"
+
+    ubyte[][] binData = [
+        cast(ubyte[]) x"12",
+        cast(ubyte[]) x"12 34",
+        cast(ubyte[]) x"12 34 56",
+        cast(ubyte[]) x"12 34 56 78",
+        cast(ubyte[]) x"12 34 56 78 90",
+        cast(ubyte[]) x"12 34 56 78 90 ab",
+        cast(ubyte[]) x"12 34 56 78 90 ab cd",
+        cast(ubyte[]) x"12 34 56 78 90 AB CD EF"
     ];
-    foreach(string hex; hexData){
-        auto bin = hexToBin( hex );
-        auto hex2 = binToHex( bin );
-        writefln("%s(len(%d)) == binToHex( %s(len(%d)) ))",hex2,hex2.length, bin, bin.length);        
-        assert(hex == binToHex( bin ));
+
+    foreach(ubyte[] bin; binData){
+        auto b1 = bin.toHex.toBin; // auto b1 = toBin(toHex(bin));
+        assert(bin == b1);
     }
+
+    assert(toHex(cast(ubyte[])x"12 34") == "1234");
+    assert(toBin("12:34:56", ":") == x"12 34 56");
+
     writefln("hexToBin(binToHex((bin)) is done");
 }
--- source/owlchain/utils/types.d
+++
@@ -1,4 +0,0 @@
-module owlchain.utils.types;
-
-enum UINT256_BYTE_LENGTH=32;
-alias uint256 = ubyte[UINT256_BYTE_LENGTH];
