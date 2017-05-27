256d7422f881515dee14697d00619abf26b6ac8a
Knolza <knolza2016@gmail.com>
2017-05-28 08:35:06 +0900

    add log file util

---
+++ source/owlchain/utils/log.d
@@ -0,0 +1,33 @@
+module owlchain.utils.log;
+
+import std.experimental.logger;
+import owlchain.utils.config;
+
+private static FileLogger _logger;
+
+static FileLogger flog() {
+    if(_logger is null){
+        _logger = new FileLogger(config.logFile);
+    } 
+    return _logger;
+}
+
+@("log")
+@system
+unittest {
+    flog.log("test");
+}
+
+// alias log       = flogger.log;
+// alias trace     = flogger.trace;
+// alias info      = flogger.info;
+// alias warning   = flogger.warning;
+// alias critical  = flogger.critical;
+
+// alias fatalf    = flogger.fatalf;
+// alias logf      = flogger.logf;
+// alias tracef    = flogger.tracef;
+// alias infof     = flogger.infof;
+// alias warningf  = flogger.warningf;
+// alias criticalf = flogger.criticalf;
+// alias fatalf    = flogger.fatalf;
