cbeff6d3f9b5d908cb4775306b1a30c88efb4dd3
Mukeun Kim <mukeunkim@gmail.com>
2017-08-22 19:12:19 +0900

    add interface of XDRStream

---
+++ source/owlchain/util/xdrStream.d
@@ -0,0 +1,20 @@
+module owlchain.util.xdrStream;
+
+/**
+ * Helper for loading a sequence of XDR objects from a file one at a time,
+ * rather than all at once.
+ */
+class XDRInputFileStream
+{
+
+
+
+}
+
+
+class XDROutputFileStream
+{
+
+
+    
+}
