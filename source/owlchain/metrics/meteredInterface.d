2a009e915bb0bfa485232db9b20ea44b73900352
sunghoon.cho <sunghoon.cho@blockchainos.org>
2017-08-25 15:48:04 +0900

    External Library Metrics Porting
    External Library Metrics Porting C ++ -> D

---
+++ source/owlchain/metrics/meteredInterface.d
@@ -0,0 +1,1 @@
+module owlchain.metrics.meteredInterface;
---
+++ source/owlchain/metrics/metricInterface.d
@@ -0,0 +1,8 @@
+module owlchain.meterics.metricInterface;
+
+import owlchain.meterics.metricProcessor;
+
+interface MetricInterface
+{
+    void Process(MetricProcessor processor);
+}
