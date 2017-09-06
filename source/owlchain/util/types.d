d9d72949246a70b70fe4622fc2721eb4526e3012
Mukeun Kim <mukeunkim@gmail.com>
2017-09-06 10:59:56 +0900

    Add function to operate on byte basis

---
+++ source/owlchain/util/types.d
@@ -0,0 +1,39 @@
+module owlchain.util.types;
+
+import owlchain.xdr;
+
+bool isZero(ref const ubyte[] b)
+{
+    for (int i = 0; i < b.length; i++)
+    {
+        if (b[i] != 0) return false;
+    }
+
+    return true;
+}
+
+bool isZero(ref const uint256 b)
+{
+    for (int i = 0; i < b.length; i++)
+    {
+        if (b[i] != 0) return false;
+    }
+
+    return true;
+}
+
+
+bool
+lessThanXored(ref const(Hash) l, ref const(Hash) r, ref const(Hash) x)
+{
+    Hash v1, v2;
+    v1.hash.length = l.hash.length;
+    v2.hash.length = l.hash.length;
+    for (size_t i = 0; i < l.hash.length; i++)
+    {
+        v1.hash[i] = x.hash[i] ^ l.hash[i];
+        v2.hash[i] = x.hash[i] ^ r.hash[i];
+    }
+
+    return v1.hash < v2.hash;
+}
