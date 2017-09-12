28b3fb1797a79759843e57d12a31096d462dc459
Mukeun Kim <mukeunkim@gmail.com>
2017-09-12 19:04:21 +0900

    update timer

--- source/owlchain/util/timer.d
+++ source/owlchain/util/timer.d
@@ -1,14 +1,18 @@
 module owlchain.util.timer;
 
 import std.datetime;
-import owlchain.xdr.type;
-import std.container.binaryheap;
+import core.thread;
+import std.container.dlist;
+import std.algorithm : sort;
 
+import owlchain.xdr.type;
 import owlchain.main.application;
 
 import owlchain.asio.ioService;
+import std.container.binaryheap;
 
 static const uint32 RECENT_CRANK_WINDOW = 1024;
+
 class VirtualClock
 {
 public:
@@ -35,7 +39,7 @@ public:
     {
         REAL_TIME,
         VIRTUAL_TIME
-    };
+    }
 
 private:
     IOService mIOService;
@@ -47,8 +51,9 @@ private:
     size_t nRealTimerCancelEvents;
     time_point mNow;
 
-    VirtualClockEvent[] mQueue;
-    BinaryHeap!(VirtualClockEvent[]) mEvents;
+    VirtualClockEvent[] mEventArray;
+    BinaryHeap!(VirtualClockEvent[], "a > b") mEvents;
+
     size_t mFlushesIgnored;
 
     bool mDestructing;
@@ -65,22 +70,71 @@ private:
 
     size_t advanceTo(time_point n)
     {
-        return 0;
+		if (mDestructing)
+		{
+			return 0;
+		}
+
+        //assert(thread_isMainThread());
+
+		// LOG(DEBUG) << "VirtualClock::advanceTo("
+		//            << n.time_since_epoch().count() << ")";
+		mNow = n;
+		VirtualClockEvent[] toDispatch;
+
+		while (!mEvents.empty)
+		{
+			if (mEvents.front.mWhen > mNow) break;
+			toDispatch ~= mEvents.front();
+			mEvents.popFront();
+		}
+
+		// Keep the dispatch loop separate from the pop()-ing loop
+		// so the triggered events can't mutate the priority queue
+		// from underneat us while we are looping.
+        for (size_t i; i < toDispatch.length; i++)
+        {
+			toDispatch[i].trigger();
+		}
+		// LOG(DEBUG) << "VirtualClock::advanceTo done";
+		maybeSetRealtimer();
+
+		return toDispatch.length;
     }
+
     size_t advanceToNext()
     {
-        return 0;
+		if (mDestructing)
+		{
+			return 0;
+		}
+		assert(mMode == Mode.VIRTUAL_TIME);
+
+        assert(thread_isMainThread());
+
+		if (mEvents.empty)
+		{
+			return 0;
+		}
+		return advanceTo(next());
     }
+
     size_t advanceToNow()
     {
-        return 0;
+		if (mDestructing)
+		{
+			return 0;
+		}
+		assert(mMode == Mode.REAL_TIME);
+		return advanceTo(now());
     }
+
 public:
     this()
     {
-        mEvents = BinaryHeap!(VirtualClockEvent[])(mQueue);
         mDestructing = false;
         mFlushesIgnored = 0;
+        mEvents = heapify!"a > b"(mEventArray);
     }
 
     size_t crank(bool block = true)
@@ -90,6 +144,30 @@ public:
 
     void noteCrankOccurred(bool hadIdle)
     {
+		// Record execution of a crank and, optionally, whether we had an idle
+		// poll event; this is used to estimate overall business of the system
+		// via recentIdleCrankPercent().
+		++mRecentCrankCount;
+		if (hadIdle)
+		{
+			++mRecentIdleCrankCount;
+		}
+
+		// Divide-out older samples once we have a suitable number of cranks to
+		// evaluate a ratio. This makes the measurement "present-biased" and
+		// the threshold (RECENT_CRANK_WINDOW) sets the size of the window (in
+		// cranks) that we consider as "the present". Using an event count
+		// rather than a time based EWMA (as in a medida::Meter) makes the
+		// ratio more precise, at the expense of accuracy of present-focus --
+		// we might accidentally consider samples from 1s, 1m or even 1h ago as
+		// "present" if the system is very lightly loaded. But since we're
+		// going to use this value to disconnect peers when overloaded, this is
+		// the preferred tradeoff.
+		if (mRecentCrankCount > RECENT_CRANK_WINDOW)
+		{
+			mRecentCrankCount >>= 1;
+			mRecentIdleCrankCount >>= 1;
+		}
     }
 
     uint32 recentIdleCrankPercent()
@@ -112,32 +190,85 @@ public:
 
     void enqueue(VirtualClockEvent ve)
     {
+		if (mDestructing)
+		{
+			return;
+		}
+        assert(thread_isMainThread());
 
+		mEvents.insert(ve);
+		maybeSetRealtimer();
     }
 
     void flushCancelledEvents()
     {
+		if (mDestructing)
+		{
+			return;
+		}
 
+		if (mFlushesIgnored <= mEvents.length)
+		{
+			// Flushing all the canceled events immediately can lead to
+			// a O(n^2) loop if many events in the queue are canceled one a time.
+			//
+			// By ignoring O(n) flushes, we batch the iterations together and
+			// restore O(n) performance.
+			mFlushesIgnored++;
+			return;
+		}
+        assert(thread_isMainThread());
+		// LOG(DEBUG) << "VirtualClock::cancelAllEventsFrom";
+
+		VirtualClockEvent[] toKeep;
+
+		while (mEvents.length != 0)
+		{
+			auto e = mEvents.front;
+			if (!e.getTriggered())
+			{
+				toKeep ~= (e);
+			}
+			mEvents.popFront();
+		}
+		mFlushesIgnored = 0;
+
+        mEvents.clear();
+        for (size_t i; i < toKeep.length; i++)
+        {
+			mEvents.insert(toKeep[i]);
+		}
+		maybeSetRealtimer();
     }
     
     bool cancelAllEvents()
     {
-        return false;
+        assert(thread_isMainThread());
+
+		bool wasEmpty = mEvents.empty;
+		while (mEvents.length != 0)
+		{
+			auto ev = mEvents.front;
+			mEvents.popFront();
+			ev.cancel();
+		}
+        mEvents.clear();
+		return !wasEmpty;
     }
 
     // only valid with VIRTUAL_TIME: sets the current value
     // of the clock
     void setCurrentTime(time_point t)
     {
-
+		assert(mMode == Mode.VIRTUAL_TIME);
+        mNow = t;
     }
 }
 
-alias bool delegate(IOErrorCode code) VirtualClockCallback;
+alias void delegate(IOErrorCode code) VirtualClockCallback;
 
 class VirtualClockEvent
 {
-
     VirtualClockCallback mCallback;
     bool mTriggered;
 
@@ -178,7 +309,7 @@ public:
 		}
     }
     
-    override int opCmp(Object o) 
+    override int opCmp(const Object o) const 
     {
 		// For purposes of priority queue, a timer is "less than"
 		// another timer if it occurs in the future (has a higher
@@ -194,7 +325,7 @@ public:
         }
         else
         {
-            return 1;
+            return  1;
         }
     }
 }
@@ -211,17 +342,24 @@ private:
 public:
     this(Application app)
     {
-
+        mClock = app.getClock();
+        mExpiryTime = mClock.now();
+        mCancelled = false;
+        mDeleting = false;
     }
 
-    this(VirtualClock app)
+    this(VirtualClock clock)
     {
-
+	    mClock = clock;
+        mExpiryTime = mClock.now();
+        mCancelled = false;
+        mDeleting = false;
     }
 
     ~this()
     {
-
+		mDeleting = true;
+		cancel();
     }
 
     VirtualClock.time_point expiry_time() const 
@@ -236,28 +374,59 @@ public:
 
     void expires_at(VirtualClock.time_point t)
     {
-
+		cancel();
+		mExpiryTime = t;
+		mCancelled = false;
     }
 
     void expires_from_now(Duration d)
     {
-
+		cancel();
+		mExpiryTime = mClock.now() + d;
+		mCancelled = false;
     }
 
     void async_wait(void delegate (IOErrorCode errorCode) fn)
     {
-
+		if (!mCancelled)
+		{
+			assert(!mDeleting);
+			auto ve = new VirtualClockEvent(mExpiryTime, seq(), fn);
+			mClock.enqueue(ve);
+			mEvents ~= (ve);
+		}
     }
 
     void async_wait(void delegate () onSuccess, void delegate (IOErrorCode errorCode) onFailure)
     {
-
-
+		if (!mCancelled)
+		{
+			assert(!mDeleting);
+			auto ve = new VirtualClockEvent(mExpiryTime, 
+                                            seq(), 
+                                            (IOErrorCode errorCode) {
+                                                         if (errorCode)
+                                                             onFailure(errorCode);
+                                                         else
+                                                             onSuccess();
+                                                     });
+			mClock.enqueue(ve);
+			mEvents ~= (ve);
+		}
     }
 
     void cancel()
     {
-
+		if (!mCancelled)
+		{
+			mCancelled = true;
+		    foreach (ref VirtualClockEvent ev; mEvents)
+			{
+				ev.cancel();
+			}
+			mClock.flushCancelledEvents();
+			mEvents.length = 0;
+		}
     }
 
     static void onFailureNoop(IOErrorCode errorCode)
